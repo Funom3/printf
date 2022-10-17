0df6492099b051de359501a7d679fab4c1946763
Funom3 <kingsleyisaackushang1997@gmail.com>
2022-10-17 13:24:57 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,74 +1,61 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- *_printf - prints formatted data to stdout
+ * _printf - formatted output conversion
  *
- *@format: string that contains the format to print
+ * @format: input string
  *
- *Return: number of characters written
- *
+ * Return: number of chars printed
  */
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
-	int i, p = 0, pc = 0;
-	int f, w, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
+	unsigned int i = 0, r = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 	{
 		return (-1);
 	}
-
-	va_start(list, format);
-
-	for (i = 0; format && format[1] != '\0'; i++)
+	if (!format[i])
 	{
-		if (format[i] != '%')
+		return (0);
+	}
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
+			if (format[i = 1] == '\0')
 			{
-				print_buffer(buffer, &buff_ind);
+				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{
+				function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+					{
+						return (-1);
+						handl_buf(buffer, format[i], ibuf), r++, i--;
+					}
+					else
+					{
+						r += function(arguments, buffer, ibuf);
+						i += ev_print_func(format, i + 1);
+					}
+				}i++;
 			}
 		}
 		else
 		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			w = get_w(format, &i, list);
-			p = get_p(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer, flags, w, p, size);
-			if (printed == -1)
-			{
-				return (-1);
-			}
-			printed_chars += printed;
+			handl_buf(buffer, format[i], ibuf), r++;
 		}
+		for (ibuf = r; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-	return (printed_chars);
-}
-
-/**
- * print_buffer - prints the content of the buffer
- *
- * @buffer: arrays of char
- *
- * @buff_ind: index
- */
-void print_buffer(char buffer[], *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-	}
-
-	*buff_ind = 0;
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (r);
 }
